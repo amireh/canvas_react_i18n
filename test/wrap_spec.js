e23f2e44ad43a3a5cb2a10f40f9843de15cd5bd7
Jon Jensen <jenseng@gmail.com>
2015-01-09 17:25:24 -0700

    wrapper refactor, fixes #2, #3
    rewrite wrapper transform on top of jsdom. this means:
    1. wrappers can be inferred from nested tags
    2. identical wrappers will be reused

--- test/wrap_spec.js
+++ test/wrap_spec.js
@@ -8,11 +8,10 @@ describe('#wrap', function() {
     var transformed = subject(input);
 
     expect(transformed.stringValue).toEqual(output);
-    expect(transformed.wrapper['*']).toEqual('<p>$1</p>');
-    expect(transformed.wrapper['**']).toEqual('<span>$1</span>');
-    expect(transformed.wrapper['***']).toEqual('<span>$1</span>');
-    expect(transformed.wrapper['****']).toEqual('<a href="http://google.com">$1</a>');
-    expect(transformed.wrapper['*****']).toEqual('<iframe src="%{page_url}">$1</iframe>');
+    expect(transformed.wrapper['*']).toEqual('<span>$1</span>');
+    expect(transformed.wrapper['**']).toEqual('<a href="http://google.com">$1</a>');
+    expect(transformed.wrapper['***']).toEqual('<iframe src="%{page_url}">$1</iframe>');
+    expect(transformed.wrapper['****']).toEqual('<p>$1</p>');
   });
 
   it('should work with a complicated DOM', function() {
@@ -22,9 +21,7 @@ describe('#wrap', function() {
 
     expect(transformed.stringValue).toEqual(output);
     expect(transformed.wrapper['*']).toEqual('<p>$1</p>');
-    expect(transformed.wrapper['**']).toEqual('<p>$1</p>');
-    expect(transformed.wrapper['***']).toEqual('<p>$1</p>');
-    expect(transformed.wrapper['****']).toEqual('<a href="%{article_url}" target="_blank">$1</a>');
+    expect(transformed.wrapper['**']).toEqual('<a href="%{article_url}" target="_blank">$1</a>');
   });
 
   it('should work with nested tags', function() {
@@ -34,21 +31,30 @@ describe('#wrap', function() {
         '<span id="one">Hi',
           '<span id="two">%{name}</span>',
         '</span>',
-      '</p>'
+      '</p>',
+      '<div>',
+        '<p>',
+          '<span>',
+            '<em>Hello again <span id="two">%{name}</span></em>',
+          '</span>',
+        '</p>',
+      '</div>',
+      '<p>No nesting here</p>'
     ].join('\n');
 
     output = subject(input);
     expect(output.stringValue).toEqual([
-      '*',
-        '**Hi',
-          '***%{name}***',
-        '**',
-      '*'
+      '**Hi',
+        '*%{name}*',
+      '**',
+      '***Hello again *%{name}****',
+      '****No nesting here****'
     ].join('\n'));
 
-    expect(output.wrapper['*']).toEqual('<p>$1</p>');
-    expect(output.wrapper['**']).toEqual('<span id="one">$1</span>');
-    expect(output.wrapper['***']).toEqual('<span id="two">$1</span>');
+    expect(output.wrapper['*']).toEqual('<span id="two">$1</span>');
+    expect(output.wrapper['**']).toEqual('<p>\n<span id="one">$1</span>\n</p>');
+    expect(output.wrapper['***']).toEqual('<div>\n<p>\n<span>\n<em>$1</em>\n</span>\n</p>\n</div>');
+    expect(output.wrapper['****']).toEqual('<p>$1</p>');
   });
 
   it('should keep the tag attributes', function() {
